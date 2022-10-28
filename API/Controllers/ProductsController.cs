79b354a4fdf6ed88473c08813827905e4ce4c090
jtnaughton <jtnaughton@deloitte.co.uk>
2022-10-28 16:57:01 +0100

    section 5 - error handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -7,8 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -29,19 +30,18 @@ namespace API.Controllers;
         }
 
         [HttpGet]
-
         public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
             
             var products = await _productsRepo.ListAsync(spec);
-
+            
             return Ok(_mapper
                 .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
         
         [HttpGet("{id}")]
-
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
@@ -56,6 +56,7 @@ namespace API.Controllers;
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrandsAsync()
         {
             return Ok(await _productBrandRepo.ListAllAsync());
+
         }
         
         [HttpGet("types")]
@@ -63,5 +64,6 @@ namespace API.Controllers;
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypesAsync()
         {
             return Ok(await _productTypeRepo.ListAllAsync());
+            
         }
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,9 @@
+using API.Errors;
+using API.Extensions;
 using API.Helpers;
-using Core.Interfaces;
+using API.Midleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -18,26 +19,20 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseSwaggerDocumentation();
+            // Called when api has no endpoint matching the request
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
